4f6bf766889d047c034c9fd68b423a837fcc081b
Mat Utter <matutter4@gmail.com>
2014-01-22 21:31:41 -0500

    Ramtex successfully generated

--- js/pixel2.js
+++ js/pixel2.js
@@ -1,235 +1,171 @@
+{ //scope
+var raw = new Array()
+var index = {}
+var cons = {}
+var ico 	= new Number
+var area 	= new Number
+var width, height
+
+function generate(imgdata) {
+	var imageObj = new Image();
+	imageObj.src = imgdata;
+	convert(imageObj);
+}
+
 function convert(image)
 {
 	var canvas = document.getElementById('myCanvas');
-	var codearea = document.getElementById('codearea');
-	var messages = document.getElementById('message_area');
+		ico = document.getElementById('icoSize').value
+		width = image.width
+		height = image.height
+		area = width*height
+		canvas.width = width
+		canvas.height = height
 	var context = canvas.getContext('2d');
-		
-		var imageX = 0, imageY = 0;
-		var width = image.width,
-			height = image.height;
-		var RGBA	= new Array(),
-			color 	= new Array(),
-			PAL 	= new Array();
-			SYM 	= new Array();
-		var collection="", r, g, b, pixel = 0;
-	if(!width || !height) return ;
-
-	context.drawImage(image, 10, 10);
+		cons = document.getElementById('message_area');
+	var collection="", r, g, b, pixel;
+	context.drawImage(image, 0, 0);
 	var data = context.getImageData(0, 0, width, height).data;
-	//var SYM = {}
-	for(var i=0, n=data.length; i<n; i+=4, pixel++ ) {
-		r = data[i];
-		g = data[i+1];
-		b = data[i+2];
+
+	index["0, 0, 0"] = 0
+	index["255, 255, 255"] = 255
+	for(var i=0, pixel=0, j=1, n=data.length; i<n; i+=4, pixel++ ) {
+		r = data[i]
+		g = data[i+1]
+		b = data[i+2]
 
 		collection =  r + ", " + g + ", " + b
+		raw[pixel] = collection
 
-		if(r+g+b != 0 && r+g+b != 255*3) {
-			color[pixel] = collection; 
-		}
-		SYM[pixel] = collection;
+		if( !(collection in index) )
+			index[collection] = j++
 	}
 
-	color	= monoFilter(color);
-	RGBA	= cssFormat(color);
-	PAL 	= palFormat(color);
-	$('#palcode').append(PAL.join("\n<br>") + "\n");
-	//$('#palcode').append("\n");
+	var palette = {}
+		palette = raw.minify()
 
-	SYM 	= colorPaletteFilter(SYM, color);
-	SYM 	= symFormat(SYM);
-	$('#symcode').append(SYM.join("\n<br>") + "\n");
+	palette.toRGBpanel()
+	palette.toPALpanel()
 
-	Say(256-color.length+" Unused palette indexes");
+	data = new Array()
+	data = buildIndicis(index, raw)
+	data.toSYMpanel()
 
-	if(RGBA.length > 256) { Say("<red>The image has more than 256 colors.</red>");
-	} else Say("<blue>The image has less than or equal then 256 colors.</blue>");
-
-	Say("H:" + height + "  W:" + width + " Colors:" + RGBA.length);
-
-	for(n=0; n < RGBA.length; n++ )
-		$('#sortable').append("<li class='ui-state-default'><box id="+n+" style='background-color:"+RGBA[n]+";'>"+n+"</box></li>");
-	
-}
-
-function filterSort(a) {
-	return a.reverse().filter( function(e, i, ary) {
-	    return ary.indexOf(e, i+1) === -1;
-	}).reverse();
-}
-
-function generate(imgdata) {
-      var imageObj = new Image();
-      imageObj.src = imgdata;
-      convert(imageObj);
-}
-
-function pauseEvent(e){
-    if(e.stopPropagation) e.stopPropagation();
-    if(e.preventDefault) e.preventDefault();
-    e.cancelBubble=true;
-    e.returnValue=false;
-    return false;
-}
-
-function Say(s) {
-	var messages = document.getElementById('message_area');
-	messages.innerHTML += "<li class='li-next'>" + s + "</li>";
-}
-
-function cssFormat(s) {
-	var i, css;
-	s = s.slice(0);
-	for(i=0;i<s.length;i++) {
-		css = s[i].match(/\d+/g).map(Number);
-		s[i] = "RGB("+css[0]+", "+css[1]+", "+css[2]+")";
+	res = "Colors: "+palette.length
+	res.toConsole()
+	res = width+"x"+height+" = "+width*height+" pixels"
+	res.toConsole()
+	res = 256 - palette.length + " indicis left."
+	res.toConsole()
+	if(raw.length != data.length) {
+		"ERROR.SYM encoding does not match length of file.".toConsole()
+		res = "Data:" + data.length + "\tIndex:" + index.length + "\tPixels:" + pixel
+			res.toConsole()
 	}
-
-	for(i=s.length-1; i<255; i++)
-		s[i] = "RGB(0, 0, 0)";
-	if(i==255) s[i] = "RGB(255, 255, 255)"
-
-	return s;
+		
 }
 
-function symFormat(s) {
-	var s2 = s.splice(0);
-	var sym = new Array();
-	var icon = 0;
-	sym[0] = "/* Element 0x0000 - Char 0x0000 */";
-	sym[1] = "{{ 0, 8, 32, 32 },{";
-
-	for(var i=2; i<s2.length; i++) {
-		sym.push(s2[i]+",");
-		if(i%(32*32)==0) {
-			sym.push("}},<br>");
-			sym.push("/* Element 0x00"+hex2(icon)+" - Char 0x00"+hex2(icon)+" */");
-			sym.push("{{ 0, 8, 32, 32 },{");
-			icon++;
-		}
-	}
-	sym[sym.length-1] = sym[sym.length-1].substring(0, sym[sym.length-1].length - 1);
-	sym.push("}}");
-	//Say();
-	//$('#symcode').append(SYM.join(",\n<br>") + "\n");
-
-	sym.unshift("/*V F 12 3 32 32 N Q  PAL<pixel2.pal>  pixel2.sym   LCDIcon FileDescriptor: Do not edit or move */\n<br>/* Put Your Comments Here */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>\n<br>\n<br>");
-	return sym;
+function buildIndicis(index, dat) {
+	var sym = new Array()
+	for(var i=0; i<dat.length; i++)
+		sym[i] = index[dat[i]].hex()
+		//$('#symcode').append("<box2 style='background-color:RGB("+dat[i]+");'></box2"); //this does something funny ;)
+	return sym
 }
-
-
-
-function palFormat(s) {
-	var i, pal;
-	s = s.slice(0); // make a shallow copy
-	for(i=0;i<s.length;i++) {
-		pal = s[i].match(/\d+/g).map(Number);
-		s[i] = "{" + hex(pal[0]) + ", " + hex(pal[1]) + ", " + hex(pal[2]) + "},";
-	}
-
-	pal = s[s.length-1];
-	for(i=s.length-1; i<255; i++)
-		s[i] = "{0x00, 0x00, 0x00},";
-	if(i==255) s[i] = pal;
-
-	s[s.length-1] = s[s.length-1].substring(0, s[s.length-1].length - 1);
-	s.unshift("{");
-	s.unshift("/*V P C 0 256 8 8 8 8 256 24 0  pixel2pal   LCDIcon FileDescriptor: Do not edit or move */");
-	s.push("}");
-	return s;
+function constructAssociation(rgb) {
+	var s = {}
+	var j = 0
+	for(var n=0; n<rgb.length; n++)
+		if(!(rgb[n] in s))
+			s[rgb[n]] = j++
+	return s
 }
-
-function hex(n) {
-	n = n.toString(16).toUpperCase();
-	if(n.length == 1) n = "0x0" + n; else n = "0x" + n;
-	return n;
-}
-
-function hex2(n) {
-	n = n.toString(16).toUpperCase();
-	if(n.length == 1) n = "0" + n;// else n = "0x" + n;
-	return n;
-}
-
-function monoFilter(s) {
-	var unique = [];
-	$.each(s, function(i, el){
-	    if($.inArray(el, unique) === -1) unique.push(el);
-	});
-	unique.splice(0,1);
-	unique.unshift("0, 0, 0");
-	unique.push("255, 255, 255");
-	return unique;
-}
-
-function utfFormat(s) {
-	//var utfstr = "¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
-	var n, i;
-	blob = new Array();
-	s = s.replace(/\{|0x|\}|\,|\/.*.\/|\s/gi," ").split(" ");//.split(/(?=(?:..)*$)/ );
-
-	for(n=0,i=0;n<s.length;n++)
-		if(s[n].match(/^[A-Z0-9]*[A-Z0-9][A-Z0-9]*$/)) { blob[i] = hex2a(s[n]); i++; }
-
-	//Say(blob.join(", "));
-	return blob;
-}
-
 function download(filename, text) {
-
-	//text = utfFormat(text);
-	
     var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=ANSI,' + encodeURIComponent(text));
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     pom.setAttribute('download', filename);
     pom.click();
 }
-
-
-
-function hex2a(hexx) {
-    var hex2 = hexx.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex2.length; i += 2)
-        str += String.fromCharCode(parseInt(hex2.substr(i, 2), 16));
-    return str;
+Array.prototype.toSYMpanel = function() {
+	var icon = {}
+	var text = ""
+	var	a, b , c , d, i
+	el = this[0].length + 1
+	i=0
+	
+	for(a=0;a<height/ico;a++) //sprites are n^2^2
+		for(b=0;b<ico;b++)
+			for(c=0;c<width/ico;c++)
+				for(d=0;d<ico;d++)
+					icon[a+","+c]+=this[i++]+",<br>\n"
+	//alert(width/ico + " " + height/ico + " " + i)
+	i=0
+	for(a=0;a<height/ico;a++)
+		for(c=0;c<width/ico;c++) {
+			text += "/* Element 0x00"+(i++).hex().replace("0x","")+" - Char 0x00"+i.hex().replace("0x","")+" */<br>\n{{ 0, 8, 32, 32 },{\n<br>"
+			text += icon[a+","+c].replace("undefined","")
+			text += "}},\n<br>\n<br>"
+		}
+		
+	$('#symcode').prepend(text.substring(0, text.length - 19))
+	$('#symcode').append("\n<br>}}\n\n");
+	$('#symcode').prepend("/*V F 12 3 32 32 N Q  PAL<pixel2.pal>  pixel2.sym   LCDIcon FileDescriptor: Do not edit or move */\n<br>/* Put Your Comments Here */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>/* */\n<br>\n<br>\n<br>");		//$('#symcode').append((i++) + " " + icon[a+","+c].length+"<br>");
 }
+Array.prototype.toPALpanel = function() {
+	$('#palcode').empty();
+	$('#palcode').append("/*V P C 0 256 8 8 8 8 256 24 0  pixel2pal   LCDIcon FileDescriptor: Do not edit or move */\n<br>{\n<br>");
+	for(var i=0; i<this.length-1; i++)
+		$('#palcode').append("{"+this[i].hex16()+"},\n<br>");
+	for(;i<255;i++)
+		$('#palcode').append("{0x00, 0x00, 0x00},\n<br>");
 
-
-function colorPaletteFilter(s, p) {
-	var s2 = s.splice(0);
-	var pal = {}
-	var sym = new Array();
-
-	for(var i=0; i<p.length; i++) 
-		pal[p[i]] = i;
-
-	//Say(pal[s2[4]]);
-
-	for(var i=0; i<s2.length; i++) 
-		sym[i] = hex(pal[s2[i]].toString());
-
-	//Say(sym.join(",\n<br>"))
-	//Say(s2[4] + " @ " + pal[s2[4]] + " " + sym.length + " total indexed pixels" );
-
-	return sym;
+	$('#palcode').append("{"+this[this.length-1]+"}\n<br>");
+	$('#palcode').append("}\n");
 }
+Array.prototype.toRGBpanel = function() {
+	$('#sortable').empty();
+	for(var i=0; i<this.length-1; i++)
+		$('#sortable').append("<li class='ui-state-default'><box style='background-color:RGB("+this[i]+");'>"+i+"</box></li>");
+	for(;i<255;i++)
+		$('#sortable').append("<li class='ui-state-default'><box style='background-color:RGB(0, 0, 0);'>"+i+"</box></li>");
 
-
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {         
-      this.splice(i, 1);
-      i--;
-    }
-  }
-  return this;
-};
-
-Array.prototype.hexit = function() {
-  for (var i = 0; i < this.length; i++) {
-  	this[i] = hex2a(this[i]);
-  }
-  return this;
-};
+	$('#sortable').append("<li class='ui-state-default'><box style='background-color:RGB("+this[this.length-1]+");'>"+i+"</box></li>");
+}
+Array.prototype.minify = function() {
+	var unique = []
+	$.each(this, function(i, el){
+	    if($.inArray(el, unique) === -1 && el!="255, 255, 255" && el!= "0, 0, 0") unique.push(el);
+	});
+	unique.unshift("0, 0, 0")
+	unique.push("255, 255, 255")
+	return unique
+}
+Array.prototype.toIndex = function() {
+	for(var i=0; i<this.length; i++)
+		color[this[i]] = i;
+}
+Array.prototype.toConsole = function() {
+		this.join("\n<br>").toConsole()
+}
+Array.prototype.toRaw = function() {
+	var s = new Array()
+	var n
+	$.each(this, function(i, el){ 
+		n = el.match(/\d+/g).map(Number);
+		s[i] = n[0] + ", " + n[1] + ", " + n[2];
+	});
+	return s
+}
+String.prototype.toConsole = function() {
+	cons.innerHTML += "<li class='li-next'>" + this + "</li>"
+}
+String.prototype.hex16 = function() {
+	var s = this.match(/\d+/g).map(Number);
+		return s[0].hex() + ", " + s[1].hex() + ", " + s[2].hex();
+}
+Number.prototype.hex = function() {
+	var s = this.toString(16).toUpperCase(); 
+	if(s.length == 2) return "0x" + s
+		else return "0x0" + s
+}
+} //end scope
